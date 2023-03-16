19f42272a9758096804fdea8eb43737cee5dafff
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-16 13:46:47 +0100

    initial commit whit base html and js const Parola

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma 
+stampare il risultato in console*/
+
+/* strumenti usati
+Const\let
+function
+prompt
+console.log
+*/
+
+// chiedi all'utente di inserire una parola
+const parola = prompt("Inserisci una parola:");
+console.log(parola);
+// definisci una funzione per verificare se la parola è palindroma
+function isPalindroma(parola) {}
