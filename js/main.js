eac72077ed62d5c721a3e15bb585db201ee5b49b
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-16 14:46:30 +0100

    add function and cicle for

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* Palidroma
+/* Palidroma :
 Chiedere all’utente di inserire una parola 
 Creare una funzione per capire se la parola inserita è palindroma 
 stampare il risultato in console*/
@@ -8,10 +8,30 @@ Const\let
 function
 prompt
 console.log
+return
+for\if
 */
 
-// chiedi all'utente di inserire una parola
-const parola = prompt("Inserisci una parola:");
-console.log(parola);
-// definisci una funzione per verificare se la parola è palindroma
-function isPalindroma(parola) {}
+// creare una function per capire se una parola è palindroma
+function palindroma(parola) {
+
+  // capisco la lunghezza della parola
+  const parolaLength = parola.length;
+
+  // creao un ciclo per capire se la parola è polindrome
+  for (let i = 0; i < parolaLength / 2; i++) {
+    /* se la parola è diversa dalla parolaLength */
+    if (parola[i] !== parola[parolaLength - 1 - i]) {
+      return "La parola non è palindroma";
+    }
+  }
+  return "La parola è palindroma";
+}
+// definizione della costante " parolaUtente"
+const parolaUtente = prompt("Inserisci una parola ");
+
+// definizione totale del ciclo
+const valore = palindroma(parolaUtente);
+
+/* console log risoltato  */
+console.log(valore);
